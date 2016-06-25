3623515fb7830e6ffd0e546dc8e7e978ad67c551
awwch <drolls4ever@gmail.com>
2016-06-25 11:13:34 +0300

    BTS

---
+++ BTS_dict_cleaner.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import re
+import nltk
+from nltk.corpus import stopwords
+import pymorphy2
+from collections import Counter
+
+stop_words = stopwords.words('russian')
+stop_words.extend(["например", "также", "нибудь", "который", "свой", "обычно", "некоторый", "кому"])
+tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+morph = pymorphy2.MorphAnalyzer()
+
+def str_key (_dict):
+    key = (str(list(_dict.keys())).strip('[]')).replace("'",'')
+    return(key)
+def str_value (_dict): 
+    value = (str(list(_dict.values())).strip('[]')).replace("'",'')
+    return(value)
+    
+def dict_to_wordlist(text, remove_stopwords=True):
+    words = []
+    gram_info = []
+    text = re.sub("[^а-яА-Я]"," ", text)
+    all_words = text.lower().split()
+    for word in all_words:
+        p = morph.parse(word)[0]
+        if word not in stop_words and  p.normal_form not in stop_words:
+            words.append({word:p.normal_form})
+            gram_info.append({p.normal_form:str(p.tag)})
+    return(words,gram_info)
+    
+def def_to_sentences(text,tokenizer, remove_stopwords=True):
+    raw_sentences = tokenizer.tokenize(text.strip())
+    sentences = []
+    for raw_sentence in raw_sentences:
+        if len(raw_sentence) > 0:
+            sentences.append(dict_to_wordlist(raw_sentence, \
+              remove_stopwords))
+    return sentences
+        
+def lemmas_in_def(lemmas,sentences):
+    c = 0
+    lemma_counter = []
+    for lemma in lemmas:
+        for sentence in sentences:
+            m = re.search(lemma,str(sentence))
+            if m != None:
+                c += 1
+        lemma_count = {lemma:c}
+        lemma_counter.append(lemma_count)
+        c = 0  
+    return(lemma_counter)
+
+defs = []
+definitions = []
+sentences = []
+lemmas = []
+normalized_forms = []
+normalized_gram = []
+nouns = []
+i = 0
+_dict = pd.read_csv( "C:\\Users\\Ania\\Desktop\\kursach\\BTS\\output1.csv", header=0, 
+ delimiter=";")
+
+for art in _dict["DEF"]:
+    defs.append([str(art)])
+for _def in defs:
+    try:
+        _def.insert(0,_dict["VOCAB"][i])
+        i += 1
+    except KeyError:
+        continue
+    definitions.append(str(_def).strip('[]'))
+del(defs)    
+
+for definition in definitions:
+    definition = definition.replace('.',';')
+    try:
+        sentences += def_to_sentences(definition, tokenizer)
+    except KeyError:
+        continue
+    
+
+for sentence in sentences:
+    
+    normalized_gram.append(sentence[-1])
+    for word in sentence[-1]:
+        word = str_key(word)        
+        normalized_forms.append(word)
+        if word not in lemmas:
+            lemmas.append(word)
+for forms in normalized_gram:
+    for word in forms:
+        line = str_value(word)
+        m = re.search('NOUN',line)
+        if m != None:
+            nouns.append(str_key(word))
+            
+c = Counter(nouns).most_common()
+f = open('nouns.csv','w',encoding = 'utf-8')
+for element in c:    
+    f.write(element[0]+'\t'+str(element[1])+'\n')
+f.close
+
+for element in c[:500]:
+    if [element[0]] not in stop_words: 
+        stop_words.extend([element[0]])
+
+#lemma_counter = lemmas_in_def(lemmas,sentences)     
+dice_dict = []
+k_words = ["абажур"]
+k_def = ''
+for sentence in sentences:
+    i = 0
+    for word in k_words:
+        under = len(sentence[0])
+        for s in sentence:
+            if len(s)>0 and str_key(s[0]) == word:
+                k_def = sentence[0]
+                for element in k_def:
+                    for d in s:
+                        if str_value(element) == str_key(d) and str_key(d) not in stop_words:
+                            i += 1
+            above = i
+            if len(s)>0:  
+                dice = {(word,str_key(s[0])):above/under}
+                if dice not in dice_dict:
+                    dice_dict.append(dice)
+
+f = open('абажур.txt','w',encoding = 'utf-8')
+for dice in dice_dict:
+    if str_value(dice) != '0.0':
+        f.write(str(dice)+'\n')
+f.close()
+
+#c = Counter(all_words_normalized).most_common()
+#f = open('frequency.csv','w',encoding = 'utf-8')
+#for line in c:
+#    f.write(line[0]+'\t'+str(line[1])+'\n')
+#f.close()
